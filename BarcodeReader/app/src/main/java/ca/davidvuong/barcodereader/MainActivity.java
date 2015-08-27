bd9010aed64b57584557d583b1aeeb56c1f1ad86
David Vuong <david.vuong@uwaterloo.ca>
2015-08-26 22:11:16 -0400

    ZXing initial commit of barcode reader

--- BarcodeReader/app/src/main/java/ca/davidvuong/barcodereader/MainActivity.java
+++ BarcodeReader/app/src/main/java/ca/davidvuong/barcodereader/MainActivity.java
@@ -1,37 +1,58 @@
 package ca.davidvuong.barcodereader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.zxing.integration.android.*;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnScan;
+    private TextView formatTxt, contentTxt;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnScan = (Button) findViewById(R.id.btnScan);
+        formatTxt = (TextView) findViewById(R.id.scan_format);
+        contentTxt = (TextView) findViewById(R.id.scan_content);
+
+        btnScan.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    public void onClick(View v) {
+        if(v.getId()==R.id.btnScan) {
+            //Initiate scan
+            IntentIntegrator scanIntegrator = new IntentIntegrator(this);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            scanIntegrator.initiateScan();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
         }
+    }
 
-        return super.onOptionsItemSelected(item);
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        //get scan result
+        IntentResult getResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (getResult != null) {
+            String scanContent = getResult.getContents();
+            String scanFormat = getResult.getFormatName();
+
+            formatTxt.setText(String.format("FORMAT: %1$s", scanFormat));
+            contentTxt.setText(String.format("CONTENT: %1$s", scanContent));
+        }
+        else {
+            Toast.makeText(this, "No data!", Toast.LENGTH_LONG).show();
+        }
     }
 }
